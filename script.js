ec95cf1f89f569185e191b5d5f23a3803ac017a1
Mayk Alexander <38488700+Maayk@users.noreply.github.com>
2024-10-21 12:07:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function getNextNightTime() {
     nextTime.setHours(hours, minutes, 0, 0);
     if (nextTime > now) return nextTime;
   }
-  // Se já passou de 23:41, retorna o próximo dia
+
   const firstTime = new Date();
   firstTime.setDate(now.getDate() + 1);
   const [hours, minutes] = nightTimes[0].split(':');
@@ -48,7 +48,7 @@ function updateHistory(time) {
   historyList.prepend(li);
 }
 
-// Notificações
+
 document.getElementById('enable-notifications').addEventListener('click', () => {
   notificationMinutes = parseInt(document.getElementById('notification-time').value);
   Notification.requestPermission().then(permission => {
@@ -64,7 +64,6 @@ document.getElementById('enable-notifications').addEventListener('click', () => 
   });
 });
 
-// Alternar entre claro/escuro
 const currentMode = localStorage.getItem('mode') || 'light';
 document.body.classList.add(`${currentMode}-mode`);
 const toggleButton = document.getElementById('toggleMode');
@@ -79,3 +78,51 @@ toggleButton.addEventListener('click', () => {
 });
 
 startCountdown();
+
+async function fetchWars() {
+  try {
+    const response = await fetch('wars.json');
+    const wars = await response.json();
+    const warsList = document.getElementById('wars-list');
+    warsList.innerHTML = '';
+
+    wars.forEach(war => {
+      const warItem = document.createElement('div');
+      warItem.className = 'war-card';
+      const warTime = new Date(war.time).toLocaleTimeString('pt-BR', { hour: '2-digit', minute: '2-digit' });
+      const TimeCount = new Date(war.time);
+      const now = new Date();
+      const timeDiff = TimeCount - now;
+      const hours = Math.floor((timeDiff / (1000 * 60 * 60)) % 24);
+      const minutes = Math.floor((timeDiff / (1000 * 60)) % 60);
+
+      const countdownText = timeDiff > 0 ? `Inicia em ${hours}h ${minutes}min` : 'A guerra já começou';
+
+      warItem.innerHTML = `
+        <div class="war-header">
+          <span>${war.attackingGuild} vs ${war.defendingGuild}</span>
+        </div>
+
+        <div class="war-details">
+          <span class="TempoGuerra">${countdownText}</span>
+          <span class="war-time">${warTime}</span>
+        </div>
+        
+        <span class="war-map">${war.map}</span><b>
+      `;
+
+      warsList.appendChild(warItem);
+    });
+  } catch (error) {
+    console.error('Erro ao carregar as guerras:', error);
+  }
+}
+
+function startAutoUpdate(interval = 60000) {
+  fetchWars();
+  setInterval(fetchWars, interval);
+}
+
+startAutoUpdate();
+
+
